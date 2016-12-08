490d2e53921c9a57e87304e072220fb253cea938
qgp9 <qgp9@rm-rf.work>
2016-12-08 03:23:25 +0200

    persist data by localStorage

--- src/index.js
+++ src/index.js
@@ -5,12 +5,16 @@ import { createStore } from 'redux';
 import { Provider } from 'react-redux';
 import usersInfo from './reducers';
 import _ from 'underscore';
+import { persistLoad, persistStore } from './utils';
 import generateUserInfo from './generateUserInfo';
 import App from './App';
 import './index.css';
 
-let store = createStore( usersInfo, {users:generateUserInfo()} );
-console.log(store.state);
+const USER_INFO_KEY = 'USER_INFO';
+const initialState = persistLoad(USER_INFO_KEY, ()=>{return { users: generateUserInfo() }} );
+let store = createStore( usersInfo, initialState );
+persistStore( USER_INFO_KEY, store.getState() );
+store.subscribe(()=>{persistStore(USER_INFO_KEY,store.getState())});
 
 ReactDOM.render(
   <Provider store={store}>
--- src/utils.js
+++ src/utils.js
@@ -3,3 +3,16 @@ import chance from 'chance';
 export function generateID(){
   return chance.guid();
 }
+
+
+export function persistLoad(key, defaultDataGenerator){
+  let localdata = localStorage.getItem(key);
+  if( localdata === 'undefined' || localdata === null ){
+    return defaultDataGenerator();
+  }else{
+    return JSON.parse(localdata);
+  }
+}
+export function persistStore(key, data){
+  localStorage.setItem(key, JSON.stringify(data));
+}
